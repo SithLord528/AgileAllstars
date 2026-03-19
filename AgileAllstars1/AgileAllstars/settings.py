5358756c25af0167f8542d8515b9e91eb51e0a1e
Molly <mollyauer@arizona.edu>
2026-03-19 09:29:12 -0700

    fixed settings

--- AgileAllstars1/AgileAllstars/settings.py
+++ AgileAllstars1/AgileAllstars/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taskStatus.apps.TaskStatusConfig'
+    'taskStatus.apps.TaskStatusConfig',
     'users.apps.UsersConfig'
 ]
 
